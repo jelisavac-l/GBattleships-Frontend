ef9810bfc7a1ebd10ebc7d90d277d464c4a28bb0
jelisavac-l <jelisavacluka63@gmail.com>
2025-08-24 01:44:09 +0200

    feat: added status screen

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <div id="app">
     <template v-if="currentPage === 'home'">
       <!-- <Home @join-clicked="showGame" @host-clicked="showGame" /> -->
-       <BoardCreator />
+       <Home @join-clicked="showGame" @host-clicked="showGameCreated"/>
     </template>
     <template v-else-if="currentPage === 'game'">
       <Game />
+    </template>
+    <template v-else-if="currentPage === 'created'">
+      <Status id="1"/>
     </template>
     <template v-else-if="currentPage === 'setup'">
       <BoardCreator />
@@ -18,17 +21,41 @@ import Game from './components/Game.vue'
 import BoardCreator from './components/BoardCreator.vue';
 import Home from './components/home/Home.vue';
 import {ref} from 'vue';
+import Status from './components/Status.vue';
 
 
 const currentPage = ref('home');
+
 const showGame = () => {
-  currentPage.value = 'setup';
+  currentPage.value = 'game';
 };
+
+const showSetup = () => {
+  currentPage.value = 'setup'
+}
+
+const showGameCreated = () => {
+  currentPage.value = 'created'
+}
+
 </script>
 
 <style scoped>
+@font-face {
+    font-family: Minecraft;
+    src: url('./assets/fonts/MinecraftRegular-Bmg3.otf');
+    font-weight: 500;
+}
+
+@font-face {
+    font-family: Minecraft;
+    src: url('./assets/fonts/MinecraftBold-nMK1.otf');
+    font-weight: bold;
+}
+
 #app {
   text-align: center;
-  font-family: 'Courier New', Courier, monospace;
+  font-family: Minecraft;
+  font-size: larger;
 }
 </style>
---
+++ src/components/Status.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="status">
+        <div v-if="id != null">
+            <p>Waiting for opponent...</p>
+            <div id="id">
+                <p>Your game id: <span id="idnumber">{{ id }}</span></p>
+            </div>
+            
+        </div>
+
+        <div v-else>
+            <p>{{ msg }}</p>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+    id: String,
+    msg: String
+})
+
+</script>
+
+<style scoped>
+
+#id {
+    padding: 0;
+    border-radius: 5px;
+    border: solid 1px;
+}
+
+#idnumber{
+    color: dodgerblue;
+    font-weight: bold;
+}
+
+#status {
+    font-size: xx-large;
+}
+
+</style>
